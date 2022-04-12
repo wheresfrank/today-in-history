022578dfb69f35834f7bce40d8705ce732835fb4
fjohnette <fjohnette@pm.me>
2022-04-12 14:34:34 -0600

    Add button to load random event

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,6 @@ class ArticlesController < ApplicationController
   def welcome
     response = (Faraday.get("https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/#{Date.today.month.to_s}/#{Date.today.day.to_s}").body)
     @articles = JSON.parse(response)['selected']
+
   end
 end
