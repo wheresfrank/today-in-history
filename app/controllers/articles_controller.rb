69816cbaf897a251772f16cddcc90c2306d413a2
fjohnette <fjohnette@pm.me>
2022-04-12 21:19:36 -0600

    redesign event page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,5 @@ class ArticlesController < ApplicationController
   def welcome
     response = (Faraday.get("https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/events/#{Date.today.month.to_s}/#{Date.today.day.to_s}").body)
     @articles = JSON.parse(response)['events']
-    @item = rand(0...@articles.length)
   end
 end
